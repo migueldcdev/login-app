1c9315bd9cb10a80fcef270a0813ca1d0f7baad9
migueldcdev <migueldc.dev@protonmail.com>
2024-09-20 19:35:54 +0000

    done middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,28 @@
+import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
+import { decodeToken } from "./app/lib/session";
 
 export default async function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL("/auth/login", request.url));
+  const sessionToken = cookies().get("session")?.value;
+  const { pathname } = new URL(request.url);
+
+  const isAuthPage = pathname === "/auth/login";
+  const isHomePage = pathname === "/home";
+
+  if (sessionToken) {
+    const data = await decodeToken(sessionToken);
+
+    if (data.payload.id && !isHomePage) {
+      return NextResponse.redirect(new URL("/home", request.url));
+    }
+  }
+  if (!sessionToken && !isAuthPage) {
+    return NextResponse.redirect(new URL("/auth/login", request.url));
+  }
 }
 
 export const config = {
-  matcher: ["/"],
+  matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+  ],
 };
