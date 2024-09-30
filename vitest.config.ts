50569b5f578854371f2078bf49538440774ba6ac
migueldcdev <migueldc.dev@protonmail.com>
2024-09-30 19:19:10 +0000

    test config to exclude playwright in vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [react()],
   test: {
     environment: "jsdom",
-  },
+    exclude: ['**/tests/**', "**/node_modules**/"],   
+  }, 
+  resolve: {
+    alias: [{ find: "@", replacement: resolve(__dirname, "./src") }]
+  }
 });
