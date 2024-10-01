3d74c8ba8dc7d0a075ac40450966baf9d79f71ff
migueldcdev <migueldc.dev@protonmail.com>
2024-10-01 20:43:57 +0000

    added style to error page

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,4 +1,4 @@
-"use client"; // Error boundaries must be Client Components
+"use client";
 
 import { useEffect } from "react";
 
@@ -9,15 +9,15 @@ export default function Error({
   error: Error & { digest?: string };
   reset: () => void;
 }) {
-  useEffect(() => {
-    // Log the error to an error reporting service
+  useEffect(() => {    
     console.error(error);
   }, [error]);
 
   return (
-    <div>
-      <h2>Something went wrong!</h2>
+    <div className="flex flex-col justify-center mt-24 text-center">
+      <p className="text-2xl">Something went wrong!</p>
       <button
+        className="underline hoveer:cursor-pointer"
         onClick={
           // Attempt to recover by trying to re-render the segment
           () => reset()
