2197fda2079ca31586aee1589033232ee9719260
migueldcdev <migueldc.dev@protonmail.com>
2024-10-01 19:27:31 +0000

    done login page unit test

--- src/app/auth/login/page.test.tsx
+++ src/app/auth/login/page.test.tsx
@@ -1,26 +1,25 @@
 import { describe, expect, test, vi } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import { userEvent } from "@testing-library/user-event";
-import { createSessionToken } from '@/lib/session';
+import { loginUser } from "@/app/actions/auth";
 
 import Login from './page';
 
+describe("Login page tests", () => {
 
-describe("Login page tests",() => {    
+    test("user should login", async () => {
+        vi.mock("@/app/actions/auth")
+        const user = userEvent;
+        render(<Login />)
 
-    test("Render Login", async() => {   
-        const spy = vi.spyOn(createSessionToken, 1)    
-        const user = userEvent;  
-        render(<Login />) 
-        
         const inputEmail = screen.getByPlaceholderText("e.g. email@example.com");
         await user.type(inputEmail, "user@email.com");
 
         const inputPassword = screen.getByPlaceholderText("********");
         await user.type(inputPassword, "userpassword");
 
-        const loginButton = screen.getByRole('button', {name: "Login"});
+        const loginButton = screen.getByRole('button', { name: "Login" });
         await user.click(loginButton);
-        
+        expect(loginUser).toBeCalled();
     })
 })
