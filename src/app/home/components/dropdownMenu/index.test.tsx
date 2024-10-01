fac0868f359aee55e22270edab1131495a464a37
migueldcdev <migueldc.dev@protonmail.com>
2024-10-01 20:36:00 +0000

    added dropdowMenu unit test

---
+++ src/app/home/components/dropdownMenu/index.test.tsx
@@ -0,0 +1,24 @@
+import { describe, expect, test, vi } from "vitest";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+import { Dropdown } from ".";
+import { logoutUser } from "@/app/actions/auth";
+
+
+describe("dropdow Menu tests",() => {
+    test("it should open and logout", async() => {
+        vi.mock("@/app/actions/auth")
+        const user = userEvent;
+        render(<Dropdown />)
+        
+        const userButton = screen.getByLabelText("Open");
+        await user.click(userButton);
+
+        const logoutButton = screen.getByLabelText("Logout");        
+
+        await user.click(logoutButton);
+
+        expect(logoutUser).toBeCalled();
+    })
+})
